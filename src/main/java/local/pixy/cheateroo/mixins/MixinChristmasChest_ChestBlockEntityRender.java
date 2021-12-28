e7262bf9721bba4f6413ca405a22a7d62c7780f3
Johannes Schmatz <johannes_schmatz@gmx.de>
2021-12-28 21:49:57 +0100

    reformat + add javadoc for mixin

--- src/main/java/local/pixy/cheateroo/mixins/MixinChristmasChest_ChestBlockEntityRender.java
+++ src/main/java/local/pixy/cheateroo/mixins/MixinChristmasChest_ChestBlockEntityRender.java
@@ -1,11 +1,8 @@
 package local.pixy.cheateroo.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import local.pixy.cheateroo.config.FeatureToggle;
-
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.enums.ChestType;
@@ -13,12 +10,22 @@ import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.render.block.entity.ChestBlockEntityRenderer;
 import net.minecraft.client.util.SpriteIdentifier;
 
+import local.pixy.cheateroo.config.FeatureToggle;
+
 /**
  * @author pixy
  *
  */
 @Mixin(ChestBlockEntityRenderer.class)
 public class MixinChristmasChest_ChestBlockEntityRender {
+	/**
+	 * A Mixin to change the texture of a chest
+	 * 
+	 * @param blockEntity
+	 * @param type
+	 * @param christmas
+	 * @return the modified texture of a chest
+	 */
 	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/TexturedRenderLayers;getChestTexture(Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/block/enums/ChestType;Z)Lnet/minecraft/client/util/SpriteIdentifier;"))
 	private SpriteIdentifier redirect(BlockEntity blockEntity, ChestType type, boolean christmas) {
 		if (FeatureToggle.OVERRIDE_CHRISTMAS_CHEST.getBooleanValue())
