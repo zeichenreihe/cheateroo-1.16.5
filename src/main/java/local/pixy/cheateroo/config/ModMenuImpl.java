017896821425594f10c046924e72c246f1013c5e
Johannes Schmatz <johannes_schmatz@gmx.de>
2021-12-31 14:46:46 +0100

    use com.terraformersmc instead of io.github.prospector

--- src/main/java/local/pixy/cheateroo/config/ModMenuImpl.java
+++ src/main/java/local/pixy/cheateroo/config/ModMenuImpl.java
@@ -1,7 +1,7 @@
 package local.pixy.cheateroo.config;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import local.pixy.cheateroo.gui.GuiConfigs;
 
 /**
